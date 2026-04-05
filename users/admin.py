e2c318b3aba240e725a15c5bfba5f01ed21aa4d8
pratik mungase <pratikmungase7385@gmail.com>
2026-04-05 17:10:10 +0530

    fix admin user 500 errr

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 
+# 🔥 CREATE FORM
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'full_name')
+
+
+# 🔥 UPDATE FORM
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'full_name', 'role', 'is_staff')
+
+
+# 🔥 ADMIN CONFIG
 class CustomUserAdmin(BaseUserAdmin):
     model = User
+
+    add_form = CustomUserCreationForm   # 🔥 IMPORTANT
+    form = CustomUserChangeForm         # 🔥 IMPORTANT
 
     list_display = ('email', 'full_name', 'role', 'is_staff')
     list_filter = ('role', 'is_staff', 'is_superuser')
@@ -12,14 +31,12 @@ class CustomUserAdmin(BaseUserAdmin):
     ordering = ('email',)
     search_fields = ('email',)
 
-    # 🔥 IMPORTANT
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('full_name',)}),
         ('Permissions', {'fields': ('role', 'is_staff', 'is_superuser')}),
     )
 
-    # 🔥 THIS FIXES 500 ERROR
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
